12218ee17aab02abe54f106b02610fcbdde6c81c
Keran <keran.mckenzie@gmail.com>
2026-05-18 14:18:18 +0930

    Initial Commit

---
+++ orbit/orbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  orbitApp.swift
+//  orbit
+//
+//  Created by Keran McKenzie on 18/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct orbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
